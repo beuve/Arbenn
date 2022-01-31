f81c6034bbc0ef4f2b9177928bb88c7a1e97b4b9
beuve <nicolas@beuve.net>
2022-01-31 21:58:54 +0100

    Up expansion tils

---
+++ lib/components/up_expension.dart
@@ -0,0 +1,332 @@
+// Copyright 2014 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flutter/material.dart';
+import '../utils/colors.dart';
+
+const Duration _kExpand = Duration(milliseconds: 100);
+
+/// A single-line [ListTile] with an expansion arrow icon that expands or collapses
+/// the tile to reveal or hide the [children].
+///
+/// This widget is typically used with [ListView] to create an
+/// "expand / collapse" list entry. When used with scrolling widgets like
+/// [ListView], a unique [PageStorageKey] must be specified to enable the
+/// [UpExpansionTile] to save and restore its expanded state when it is scrolled
+/// in and out of view.
+///
+/// This class overrides the [ListTileTheme.iconColor] and [ListTileTheme.textColor]
+/// theme properties for its [ListTile]. These colors animate between values when
+/// the tile is expanded and collapsed: between [iconColor], [collapsedIconColor] and
+/// between [textColor] and [collapsedTextColor].
+///
+/// The expansion arrow icon is shown on the right by default in left-to-right languages
+/// (i.e. the trailing edge). This can be changed using [controlAffinity]. This maps
+/// to the [leading] and [trailing] properties of [UpExpansionTile].
+///
+/// {@tool dartpad}
+/// This example demonstrates different configurations of UpExpansionTile.
+///
+/// ** See code in examples/api/lib/material/expansion_tile/expansion_tile.0.dart **
+/// {@end-tool}
+///
+/// See also:
+///
+///  * [ListTile], useful for creating expansion tile [children] when the
+///    expansion tile represents a sublist.
+///  * The "Expand and collapse" section of
+///    <https://material.io/components/lists#types>
+class _UpExpansionTile extends StatefulWidget {
+  /// Creates a single-line [ListTile] with an expansion arrow icon that expands or collapses
+  /// the tile to reveal or hide the [children]. The [initiallyExpanded] property must
+  /// be non-null.
+  const _UpExpansionTile({
+    Key? key,
+    required this.header,
+    this.onExpansionChanged,
+    required this.child,
+    this.initiallyExpanded = false,
+    this.maintainState = false,
+    this.tilePadding,
+    this.expandedCrossAxisAlignment,
+    this.expandedAlignment,
+    this.childrenPadding,
+    this.backgroundColor,
+    this.textColor,
+    this.collapsedIconColor,
+  })  : assert(
+          expandedCrossAxisAlignment != CrossAxisAlignment.baseline,
+          'CrossAxisAlignment.baseline is not supported since the expanded children '
+          'are aligned in a column, not a row. Try to use another constant.',
+        ),
+        super(key: key);
+
+  /// The primary content of the list item.
+  ///
+  /// Typically a [Text] widget.
+  final Widget header;
+
+  /// Called when the tile expands or collapses.
+  ///
+  /// When the tile starts expanding, this function is called with the value
+  /// true. When the tile starts collapsing, this function is called with
+  /// the value false.
+  final ValueChanged<bool>? onExpansionChanged;
+
+  /// The widgets that are displayed when the tile expands.
+  ///
+  /// Typically [ListTile] widgets.
+  final Widget child;
+
+  /// The color to display behind the sublist when expanded.
+  final Color? backgroundColor;
+
+  /// Specifies if the list tile is initially expanded (true) or collapsed (false, the default).
+  final bool initiallyExpanded;
+
+  /// Specifies whether the state of the children is maintained when the tile expands and collapses.
+  ///
+  /// When true, the children are kept in the tree while the tile is collapsed.
+  /// When false (default), the children are removed from the tree when the tile is
+  /// collapsed and recreated upon expansion.
+  final bool maintainState;
+
+  /// Specifies padding for the [ListTile].
+  ///
+  /// Analogous to [ListTile.contentPadding], this property defines the insets for
+  /// the [leading], [title], [subtitle] and [trailing] widgets. It does not inset
+  /// the expanded [children] widgets.
+  ///
+  /// When the value is null, the tile's padding is `EdgeInsets.symmetric(horizontal: 16.0)`.
+  final EdgeInsetsGeometry? tilePadding;
+
+  /// Specifies the alignment of [children], which are arranged in a column when
+  /// the tile is expanded.
+  ///
+  /// The internals of the expanded tile make use of a [Column] widget for
+  /// [children], and [Align] widget to align the column. The `expandedAlignment`
+  /// parameter is passed directly into the [Align].
+  ///
+  /// Modifying this property controls the alignment of the column within the
+  /// expanded tile, not the alignment of [children] widgets within the column.
+  /// To align each child within [children], see [expandedCrossAxisAlignment].
+  ///
+  /// The width of the column is the width of the widest child widget in [children].
+  ///
+  /// When the value is null, the value of `expandedAlignment` is [Alignment.center].
+  final Alignment? expandedAlignment;
+
+  /// Specifies the alignment of each child within [children] when the tile is expanded.
+  ///
+  /// The internals of the expanded tile make use of a [Column] widget for
+  /// [children], and the `crossAxisAlignment` parameter is passed directly into the [Column].
+  ///
+  /// Modifying this property controls the cross axis alignment of each child
+  /// within its [Column]. Note that the width of the [Column] that houses
+  /// [children] will be the same as the widest child widget in [children]. It is
+  /// not necessarily the width of [Column] is equal to the width of expanded tile.
+  ///
+  /// To align the [Column] along the expanded tile, use the [expandedAlignment] property
+  /// instead.
+  ///
+  /// When the value is null, the value of `expandedCrossAxisAlignment` is [CrossAxisAlignment.center].
+  final CrossAxisAlignment? expandedCrossAxisAlignment;
+
+  /// Specifies padding for [children].
+  ///
+  /// When the value is null, the value of `childrenPadding` is [EdgeInsets.zero].
+  final EdgeInsetsGeometry? childrenPadding;
+
+  /// The icon color of tile's expansion arrow icon when the sublist is collapsed.
+  ///
+  /// Used to override to the [ListTileTheme.iconColor].
+  final Color? collapsedIconColor;
+
+  /// The color of the tile's titles when the sublist is expanded.
+  ///
+  /// Used to override to the [ListTileTheme.textColor].
+  final Color? textColor;
+
+  @override
+  State<_UpExpansionTile> createState() => UpTileState();
+}
+
+class UpTileState extends State<_UpExpansionTile>
+    with SingleTickerProviderStateMixin {
+  static final Animatable<double> _easeInTween =
+      CurveTween(curve: Curves.linear);
+
+  late AnimationController _controller;
+  late Animation<double> _heightFactor;
+
+  bool _isExpanded = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(duration: _kExpand, vsync: this);
+    _heightFactor = _controller.drive(_easeInTween);
+
+    _isExpanded = PageStorage.of(context)?.readState(context) as bool? ??
+        widget.initiallyExpanded;
+    if (_isExpanded) _controller.value = 1.0;
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  void expand() => _setExpanded(true);
+
+  void collapse() => _setExpanded(false);
+
+  void toggle() => _setExpanded(!_isExpanded);
+
+  void updatePosition(Offset o) {
+    _controller.animateTo(_heightFactor.value - o.direction * o.distance / 10);
+  }
+
+  void _setExpanded(bool isExpanded) {
+    if (_isExpanded != isExpanded) {
+      setState(() {
+        _isExpanded = !_isExpanded;
+        if (_isExpanded) {
+          _controller.forward();
+        } else {
+          _controller.reverse().then<void>((void value) {
+            if (!mounted) return;
+            setState(() {
+              // Rebuild without widget.children.
+            });
+          });
+        }
+        PageStorage.of(context)?.writeState(context, _isExpanded);
+      });
+    }
+  }
+
+  void _handleTap() {
+    setState(() {
+      _isExpanded = !_isExpanded;
+      if (_isExpanded) {
+        _controller.forward();
+      } else {
+        _controller.reverse().then<void>((void value) {
+          if (!mounted) return;
+          setState(() {
+            // Rebuild without widget.children.
+          });
+        });
+      }
+      PageStorage.of(context)?.writeState(context, _isExpanded);
+    });
+    widget.onExpansionChanged?.call(_isExpanded);
+  }
+
+  Widget _buildChildren(BuildContext context, Widget? child) {
+    return Container(
+      decoration: BoxDecoration(
+        color: widget.backgroundColor ?? Colors.transparent,
+      ),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          GestureDetector(
+            onVerticalDragUpdate: (dragUpdate) {
+              updatePosition(dragUpdate.delta);
+            },
+            onTap: _handleTap,
+            child: widget.header,
+          ),
+          ClipRect(
+            child: Align(
+              alignment: widget.expandedAlignment ?? Alignment.center,
+              heightFactor: _heightFactor.value,
+              child: child,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final bool closed = !_isExpanded && _controller.isDismissed;
+    final bool shouldRemoveChildren = closed && !widget.maintainState;
+
+    final Widget result = Offstage(
+      offstage: closed,
+      child: TickerMode(
+        enabled: !closed,
+        child: Padding(
+          padding: widget.childrenPadding ?? EdgeInsets.zero,
+          child: widget.child,
+        ),
+      ),
+    );
+
+    return AnimatedBuilder(
+      animation: _controller.view,
+      builder: _buildChildren,
+      child: shouldRemoveChildren ? null : result,
+    );
+  }
+}
+
+Widget tileUp({
+  required Nuance color,
+  required GlobalKey<dynamic> key,
+  required Widget child,
+  required List<GlobalKey<dynamic>> concurrentKeys,
+  required String label,
+  double headerHeight = 80,
+  Color? background,
+}) {
+  return _UpExpansionTile(
+    key: key,
+    backgroundColor: background,
+    header: Container(
+      width: double.infinity,
+      height: headerHeight,
+      alignment: Alignment.center,
+      decoration: BoxDecoration(
+          color: color.lighter,
+          borderRadius: const BorderRadius.only(
+            topLeft: Radius.circular(30),
+            topRight: Radius.circular(30),
+          )),
+      child: Column(
+        children: [
+          Container(
+            width: 100,
+            height: 5,
+            margin: const EdgeInsets.symmetric(vertical: 10, horizontal: 0),
+            decoration: BoxDecoration(
+              color: color.darker,
+              borderRadius: const BorderRadius.all(
+                Radius.circular(10),
+              ),
+            ),
+          ),
+          Text(
+            label,
+            style: TextStyle(fontSize: 30, color: color.darker),
+          ),
+        ],
+      ),
+    ),
+    onExpansionChanged: (isActive) {
+      for (var key in concurrentKeys) {
+        key.currentState?.collapse();
+      }
+    },
+    child: Container(
+      decoration: BoxDecoration(color: color.lighter),
+      child: child,
+    ),
+  );
+}

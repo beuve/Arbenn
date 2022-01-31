4acdd151b32439f03e2543aad464a90f49f7b164
beuve <nicolas@beuve.net>
2022-01-31 22:07:21 +0100

    Show sign page at startup

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@
 // | $$  | $$| $$  | $$| $$$$$$$/| $$$$$$$$| $$ \  $$| $$ \  $$
 // |__/  |__/|__/  |__/|_______/ |________/|__/  \__/|__/  \__/
 
+import 'package:arbenn/pages/sign_page.dart';
 import 'package:flutter/material.dart';
 
 void main() async {
@@ -52,6 +53,6 @@ class _MyHomePageState extends State<MyHomePage> {
     // The Flutter framework has been optimized to make rerunning build methods
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
-    return const Text("");
+    return SignPage();
   }
 }

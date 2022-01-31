70b203e6cee5d88fd55f4bed153fe257ce93ca15
beuve <nicolas@beuve.net>
2022-01-31 22:11:08 +0100

    Add Date picker and search input

--- lib/components/inputs.dart
+++ lib/components/inputs.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../utils/colors.dart';
 
 class FormInput extends StatelessWidget {
   final String label;
@@ -31,6 +32,97 @@ class FormInput extends StatelessWidget {
       maxLines: minLines != null && maxLines < minLines! ? minLines : maxLines,
       minLines: minLines,
       autofocus: autoFocus,
+      decoration: InputDecoration(
+        enabledBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: color, width: 2),
+            borderRadius: const BorderRadius.all(Radius.circular(30.0))),
+        focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: color, width: 2),
+            borderRadius: const BorderRadius.all(Radius.circular(30.0))),
+        disabledBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: color, width: 2),
+            borderRadius: const BorderRadius.all(Radius.circular(30.0))),
+        labelText: label,
+        labelStyle: TextStyle(color: color, fontSize: 20),
+        contentPadding: const EdgeInsets.all(25),
+      ),
+      style: TextStyle(color: color, fontSize: 20),
+    );
+  }
+}
+
+class SearchInput extends StatelessWidget {
+  final String label;
+  final Nuance color;
+  final bool autoFocus;
+  final int? minLines;
+  final int maxLines;
+  final TextInputType keyboardType;
+  final bool isSmallDevice;
+
+  const SearchInput({
+    required this.label,
+    required this.color,
+    this.autoFocus = false,
+    this.minLines,
+    this.maxLines = 1,
+    this.isSmallDevice = false,
+    this.keyboardType = TextInputType.text,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      autocorrect: false,
+      autofocus: autoFocus,
+      keyboardType: keyboardType,
+      decoration: InputDecoration(
+        border: const UnderlineInputBorder(),
+        enabledBorder: UnderlineInputBorder(
+            borderSide: BorderSide(color: color.dark, width: 2)),
+        focusedBorder: UnderlineInputBorder(
+            borderSide: BorderSide(color: color.dark, width: 2)),
+        disabledBorder: UnderlineInputBorder(
+            borderSide: BorderSide(color: color.dark, width: 2)),
+        hintText: label,
+        hintStyle: TextStyle(color: color.main, fontSize: 20),
+      ),
+      style: TextStyle(color: color.dark, fontSize: 20),
+    );
+  }
+}
+
+class DatePicker extends StatelessWidget {
+  final Color color;
+  final String label;
+  final TextEditingController controller;
+
+  const DatePicker({
+    Key? key,
+    required this.color,
+    required this.label,
+    required this.controller,
+  }) : super(key: key);
+
+  void _selectDate(BuildContext context) async {
+    final DateTime? picked = await showDatePicker(
+        context: context,
+        initialDate: DateTime.now(),
+        firstDate: DateTime(2015, 8),
+        lastDate: DateTime(2101));
+    if (picked != null) {
+      controller.text = "${picked.day} / ${picked.month} / ${picked.year}";
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      autocorrect: false,
+      onTap: () => _selectDate(context),
+      readOnly: true,
+      controller: controller,
       decoration: InputDecoration(
         enabledBorder: OutlineInputBorder(
             borderSide: BorderSide(color: color, width: 2),

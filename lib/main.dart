f2f841d8aefb0e97ca8b19569af09c50d714bd2e
beuve <nicolas@beuve.net>
2022-01-31 23:17:13 +0100

    Use firebase core package and initialize firebase in main

--- lib/main.dart
+++ lib/main.dart
@@ -9,8 +9,11 @@
 
 import 'package:arbenn/pages/sign_page.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 

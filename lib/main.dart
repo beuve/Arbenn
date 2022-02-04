7efede8f117122033a5736ea3f2eb22f6ca3e72c
beuve <nicolas@beuve.net>
2022-02-04 22:59:33 +0100

    Change application start page based on user sign up advancement

--- lib/main.dart
+++ lib/main.dart
@@ -7,9 +7,12 @@
 // | $$  | $$| $$  | $$| $$$$$$$/| $$$$$$$$| $$ \  $$| $$ \  $$
 // |__/  |__/|__/  |__/|_______/ |________/|__/  \__/|__/  \__/
 
+import 'package:arbenn/pages/nav.dart';
 import 'package:arbenn/pages/sign_page.dart';
+import 'package:arbenn/pages/user_form.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -47,15 +50,41 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
+enum UserState { signedOut, signedIn, emailVerified, onboardingDone }
+
 class _MyHomePageState extends State<MyHomePage> {
+  late User? _user;
+
+  UserState getUserState() {
+    if (_user == null) {
+      return UserState.signedOut;
+    } else if (_user!.emailVerified) {
+      return UserState.emailVerified;
+    }
+    return UserState.signedIn;
+  }
+
+  @override
+  initState() {
+    super.initState();
+    FirebaseAuth.instance.signOut();
+    _user = FirebaseAuth.instance.currentUser;
+    FirebaseAuth.instance.authStateChanges().listen((User? user) {
+      setState(() => _user = user);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return SignPage();
+    final UserState userState = getUserState();
+
+    if (userState == UserState.signedOut) {
+      return SignPage();
+    } else if (userState == UserState.signedIn) {
+      return const EmailValidationPage(nextPage: UserFormPage());
+    } else if (userState == UserState.emailVerified) {
+      return const UserFormPage();
+    }
+    return const Nav();
   }
 }
